7034722c88abc39d7f568b8ef5e1b95c1fc89887
John Kim <john@gymtinc.com>
2022-01-13 16:54:12 +0900

    feat: navigation 추가

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,83 +1,16 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  ScrollView,
-  FlatList,
-  Modal,
-} from 'react-native';
-import TouchableOpacityButton from '../components/TouchableOpacityButton';
-import TouchableHighlightButton from '../components/TouchableHighlightButton';
-import { useState } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StackParamList } from '../App';
 
-const Home = (props: any) => {
-  const [modal, setModal] = useState(false);
+type Props = NativeStackScreenProps<StackParamList, 'Home'>;
 
-  const handlePress = () => {
-    props.navigation.navigate('Shop');
-  };
-
+const Home = (props: Props) => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={{ color: 'dodgerblue' }}>Inline Style</Text>
-      </View>
-
-      <View>
-        <TextInput placeholder="이름" />
-      </View>
-
-      <ScrollView style={{ height: 80 }}>
-        <Text>적은 수의 Item 1</Text>
-        <Text>적은 수의 Item 2</Text>
-        <Text>적은 수의 Item 3</Text>
-        <Text>적은 수의 Item 4</Text>
-        <Text>적은 수의 Item 5</Text>
-        <Text>적은 수의 Item 6</Text>
-      </ScrollView>
-
-      {/* ScrollView는 모든 것을 렌더링 하기 때문에 긴리스트 인 경우에는 FlatList를 사용해야 한다. */}
-      <FlatList
-        keyExtractor={(item) => item}
-        data={[
-          '많은 수의 Item 1',
-          '많은 수의 Item 2',
-          '많은 수의 Item 3',
-          '많은 수의 Item 4',
-          '많은 수의 Item 5',
-          '많은 수의 Item 6',
-        ]}
-        renderItem={(itemData) => (
-          <View>
-            <Text>{itemData.item}</Text>
-          </View>
-        )}
-        style={{ height: 80 }}
+      <Button
+        title="다른 화면으로 이동"
+        onPress={() => props.navigation.navigate('Other')}
       />
-
-      <View>
-        <TouchableOpacityButton title="TouchableOpacity" onPress={() => {}} />
-        <TouchableHighlightButton
-          title="TouchableHighlight"
-          onPress={() => {}}
-        />
-      </View>
-
-      <View>
-        <Button title="Open" onPress={() => setModal(true)} />
-      </View>
-
-      <Modal visible={modal} animationType="slide">
-        <View style={styles.modalContainer}>
-          <Button title="Close" onPress={() => setModal(false)} />
-        </View>
-      </Modal>
-
-      <View>
-        <Button title="이동" onPress={handlePress} />
-      </View>
     </View>
   );
 };
@@ -86,8 +19,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     padding: 20,
-  },
-  modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
--- screens/Other.tsx
+++ screens/Other.tsx
@@ -1,9 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+  Modal,
+} from 'react-native';
+import TouchableOpacityButton from '../components/TouchableOpacityButton';
+import TouchableHighlightButton from '../components/TouchableHighlightButton';
+import { useState } from 'react';
+import { StackParamList } from '../App';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-const Other = (props: any) => {
+type Props = NativeStackScreenProps<StackParamList, 'Other'>;
+
+const Other = (props: Props) => {
+  const [modal, setModal] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Other</Text>
+      <View>
+        <Text style={{ color: 'dodgerblue' }}>Inline Style</Text>
+      </View>
+
+      <View>
+        <TextInput placeholder="이름" />
+      </View>
+
+      <ScrollView style={{ height: 80 }}>
+        <Text>적은 수의 Item 1</Text>
+        <Text>적은 수의 Item 2</Text>
+        <Text>적은 수의 Item 3</Text>
+        <Text>적은 수의 Item 4</Text>
+        <Text>적은 수의 Item 5</Text>
+        <Text>적은 수의 Item 6</Text>
+      </ScrollView>
+
+      {/* ScrollView는 모든 것을 렌더링 하기 때문에 긴리스트 인 경우에는 FlatList를 사용해야 한다. */}
+      <FlatList
+        keyExtractor={(item) => item}
+        data={[
+          '많은 수의 Item 1',
+          '많은 수의 Item 2',
+          '많은 수의 Item 3',
+          '많은 수의 Item 4',
+          '많은 수의 Item 5',
+          '많은 수의 Item 6',
+        ]}
+        renderItem={(itemData) => (
+          <View>
+            <Text>{itemData.item}</Text>
+          </View>
+        )}
+        style={{ height: 80 }}
+      />
+
+      <View>
+        <TouchableOpacityButton title="TouchableOpacity" onPress={() => {}} />
+        <TouchableHighlightButton
+          title="TouchableHighlight"
+          onPress={() => {}}
+        />
+      </View>
+
+      <View>
+        <Button title="모달 열기" onPress={() => setModal(true)} />
+      </View>
+
+      <Modal visible={modal} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Button title="모달 닫기" onPress={() => setModal(false)} />
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -12,6 +82,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     padding: 20,
+  },
+  modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
